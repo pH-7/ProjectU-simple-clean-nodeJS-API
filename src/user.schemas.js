a03484521c64c5a43c49e7d9bb6b339a4c8970a3
Pierre-Henry Soria <hi@ph7.me>
2022-07-11 00:44:43 +0930

    Validate min/max length for `name`, `city`, `country`

--- src/user.schemas.js
+++ src/user.schemas.js
@@ -1,5 +1,18 @@
 import * as yup from 'yup'
 
+const MIN_LENGTH = {
+    name: 2,
+    city: 1,
+    country: 2
+};
+
+const MAX_LENGTH = {
+    name: 20,
+    city: 30,
+    country: 30,
+    email: 100
+};
+
 export const getUser = {
     schema: {
         params: {
@@ -14,10 +27,10 @@ export const addUser = {
     schema: {
         body: {
             yupSchema: yup.object().shape({
-                name: yup.string(),
-                email: yup.string().email(),
-                city: yup.string(),
-                country: yup.string(),
+                name: yup.string().min(MIN_LENGTH.name).max(MAX_LENGTH.name),
+                email: yup.string().email().max(MAX_LENGTH.email),
+                city: yup.string().min(MIN_LENGTH.city).max(MAX_LENGTH.city),
+                country: yup.string().min(MIN_LENGTH.country).max(MAX_LENGTH.country),
             })
         },
     },
@@ -32,10 +45,10 @@ export const updateUser = {
         },
         body: {
             yupSchema: yup.object().shape({
-                name: yup.string(),
+                name: yup.string().min(MIN_LENGTH.name).max(MAX_LENGTH.name),
                 email: yup.string().email(),
-                city: yup.string(),
-                country: yup.string(),
+                city: yup.string().min(MIN_LENGTH.city).max(MAX_LENGTH.city),
+                country: yup.string().min(MIN_LENGTH.country).max(MAX_LENGTH.country),
             })
         },
     },
