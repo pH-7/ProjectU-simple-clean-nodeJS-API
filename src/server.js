a80c91a7059584bf060043ec4a64834a672d1b64
Pierre-Henry Soria <hi@ph7.me>
2022-07-14 18:34:41 +0930

    Compress requests and rate limit endpoints

--- src/server.js
+++ src/server.js
@@ -4,10 +4,21 @@ import cors from 'cors';
 
 import mainRoutes from './main.routes';
 import userRoutes from './user.routes';
+import rateLimit from "express-rate-limit";
+import compression from "compression";
 
 const app = express();
 const port = 4000;
 
+const limiter = rateLimit({
+    windowMs: 60 * 1000, // 1 min
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+})
+
+app.use(compression())
+
+// Apply the rate limiting middleware to API calls only
+app.use(limiter)
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
