82032a3704d5dd7405f230e994e4cf96a51f6f0a
♚ PH⑦ de Soria™♛ <hi@ph7.me>
2023-03-10 19:41:50 +1100

    Update users.data.js

--- src/models/data/users.data.js
+++ src/models/data/users.data.js
@@ -1,6 +1,6 @@
 
-// format defined in Joi validation schema user.schema.js
-// { name, email, city, country}
+// format defined in Joi validation schema user.schemas.js
+// { name, email, city, country }
 const users = [];
 
 export default users;
