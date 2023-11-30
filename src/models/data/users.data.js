cb857042755fe6169098b41134d606b72cac5f7c
♚ PH⑦ de Soria™♛ <ph7software@gmail.com>
2023-12-01 10:13:28 +1100

    Remove redundant comment

--- src/models/data/users.data.js
+++ src/models/data/users.data.js
@@ -1,4 +1,4 @@
-// an array on objects, where the schema rules are defined in user.schemas.js Joi's schema
+// an array on objects, where the schema rules are defined in user.schemas.js
 // { name, email, city, country }
 const users = [];
 
