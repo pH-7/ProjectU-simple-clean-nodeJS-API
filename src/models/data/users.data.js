ebe305a50aa1d990214be3438aa0be1383ad7bee
♚ PH⑦ de Soria™♛ <ph7software@gmail.com>
2023-12-01 10:12:35 +1100

    Update comment re. the object

--- src/models/data/users.data.js
+++ src/models/data/users.data.js
@@ -1,5 +1,4 @@
-
-// format defined in Joi validation schema user.schemas.js
+// an array on objects, where the schema rules are defined in user.schemas.js Joi's schema
 // { name, email, city, country }
 const users = [];
 
