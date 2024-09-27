e6ca71ff3fb7616eeaa4af68246e2524d0ed2bfb
♚ PH⑦ de Soria™♛ <hi@ph7.me>
2024-09-27 18:14:39 +1000

    Give better details in comment for helping my Udemy students

--- src/models/data/users.data.js
+++ src/models/data/users.data.js
@@ -1,4 +1,4 @@
-// an array on objects, where the schema rules are defined in user.schemas.js
+// Persistent memory in an array of objects, where the schema rules are defined in user.schemas.js
 // { name, email, city, country }
 const users = [];
 
