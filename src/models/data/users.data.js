868a35024d26459871d5660adbce4f551a3a33b8
♚ PH⑦ — Pierre-Henry™♛ <hi@ph7.me>
2026-06-25 09:35:29 +1000

    Explain another way of persisting data for a more industry-production-ready system with a non-relational MongoDB example

--- src/models/data/users.data.js
+++ src/models/data/users.data.js
@@ -1,5 +1,7 @@
 // Persistent memory in an array of objects, where the schema rules are defined in user.schemas.js
 // { name, email, city, country }
+// FYI, we could have also used something like mongodb, with "mongodb-memory-server" for instance, for this use case
+// https://www.npmjs.com/package/mongodb-memory-server
 const users = [];
 
 export default users;
